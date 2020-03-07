2b2314859499a5dab738cebb7132fa88a34ef759
Viirumäki Jarno <jarno.viirumaki@epec.fi>
2020-03-07 13:42:54 +0200

    Added process.env.port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
-const port = 8081
+const port = process.env.port || 8081 //Lisättiin portti joka lukee myös palvelimen portin
 
 ws.createServer({ server: httpServer }, aedes.handle)
 
