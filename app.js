4404cf4c62bbe1280dec244d66629fa7b9ab79cb
Viirumäki Jarno <jarno.viirumaki@epec.fi>
2020-03-07 13:47:56 +0200

    port changed to PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
-const port = process.env.port || 8081 //Lisättiin portti joka lukee myös palvelimen portin
+const port = process.env.PORT || 8081 //Lisättiin portti joka lukee myös palvelimen portin
 
 ws.createServer({ server: httpServer }, aedes.handle)
 
